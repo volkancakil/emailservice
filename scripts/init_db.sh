0cc095f3e6a87df3f1fb8344f440b69f9e20b6ac
Volkan Cakil <volkancakil@gmail.com>
2021-09-21 06:39:57 +0300

    Docker port binding 5432:5432 to 5432

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -29,7 +29,7 @@ docker run \
   -e POSTGRES_USER=${DB_USER} \
   -e POSTGRES_PASSWORD=${DB_PASSWORD} \
   -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
+  -p "${DB_PORT}" \
   -d postgres \
   postgres -N 1000
   # ^ Increased maximum number of connections for testing purposes
